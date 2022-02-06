f2148be43f12178d4f9bd738b0046626064ae572
Markos <12499gr@saeinstitute.edu>
2022-02-06 23:18:32 +0200

    Player's Movement & Mian Camera
    -Player's Movement is complete with the addition of player's dash which replaced the double jump feature. [Finalized the values for now].
    -Created the game's main camera using unity's cinemachine and create a script which causes a camera shake effect when the player contacts a trap. [Finalized the values for now].

---
+++ Escaping Death Game/Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Events;
+
+public class CameraShake : MonoBehaviour // This Script will Make the Camera Shake when the player contacts a trap 
+{
+    public float shakeDuration = 0.2f;      // How much time the Camera will Shake
+    public float shakeWidth = 1.0f;         // Shake's width
+    public float shakeFrequency = 1.5f;     // Shake's frequency
+    private float shakeElapsedTime = 0.0f;
+
+    // Camera Shake (Cinemachine Shake) using Noise
+    public CinemachineVirtualCamera virtualCamera;
+    private CinemachineBasicMultiChannelPerlin virtualCameraNoise;
+
+    void Start()
+    {
+        if (virtualCamera != null)
+        {
+            virtualCameraNoise = virtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+        }
+    }
+
+    void Update()
+    {
+        if (virtualCamera != null && virtualCameraNoise != null)
+        {
+            // When the shake effect is enabled
+            if (shakeElapsedTime > 0)
+            {
+                // Camera's shake main parameters
+                virtualCameraNoise.m_AmplitudeGain = shakeWidth;
+                virtualCameraNoise.m_FrequencyGain = shakeFrequency;
+
+                // Shake Timer Update Continuously
+                shakeElapsedTime -= Time.deltaTime;
+            }
+            else
+            {
+                // Reset the main parameters If the Camera Shake Effect has Finished
+                virtualCameraNoise.m_AmplitudeGain = 0f;
+                shakeElapsedTime = 0f; // No Camera Shake Anymore
+            }
+        }
+    }
+
+    public void ShakeEnabled()  // Calling this Function from a Different Script will Enable Camera Shake
+    {
+        shakeElapsedTime = shakeDuration;
+    }
+}
