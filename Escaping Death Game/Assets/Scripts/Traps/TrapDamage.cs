d09e8fd5e3563a3991548e77fb72340a497bd28a
Markos <12499gr@saeinstitute.edu>
2022-02-07 15:03:33 +0200

    Traps & Checkpoints
    -Traps when the players contact them they will transform (punish) them to the starting area or on the latest checkpoint. Script and functionality is ready and can be applied to every trap that the game will have.

--- Escaping Death Game/Assets/Scripts/Traps/TrapDamage.cs
+++ Escaping Death Game/Assets/Scripts/Traps/TrapDamage.cs
@@ -2,28 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapDamage : MonoBehaviour
+public class TrapDamage : MonoBehaviour // Traps won't do any real damage to the players only to "transform" them to start from the beginning or from the latest checkpoint (This is the game's traps punish variable)
 {
-    public CameraShake cameraShake;
+    public CameraShake cameraShake; // In order to access the camera shake variable from the camera shake script
+    public GameObject resetArea; // Reset area could be the start or the checkpoint of the level depending on where the player is
+    private GameObject playerGO;
+    public Rigidbody2D playerRB;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        playerGO = GameObject.Find("Player"); // In order to assign it automatically in each
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            cameraShake.ShakeEnabled();
-            Debug.Log("Trap Damage");
+            cameraShake.ShakeEnabled(); // When the players contact a trap, a camera shake will appear in order to strengthen the trap damage/punish effect
+
+            playerRB.velocity = new Vector2(0, 0); // After the Player Respawns he Doesn't Inherit any Previous Values on X and Y Axis
+            playerGO.transform.position = resetArea.transform.position; // Player will transform to the reset area
         }
     }
 }
