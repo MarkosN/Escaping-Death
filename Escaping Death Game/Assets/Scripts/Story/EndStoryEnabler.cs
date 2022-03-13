7cf2b13870346e4e249ca0a0fcecc5f2fcce14cb
Markos <12499gr@saeinstitute.edu>
2022-03-13 23:14:26 +0200

    Story 2
    -The game's end scene of the game takes place in the same location as the start scene where the player comes back to life which will be shown by waking up the player and fade slowly the death. After this the player is tranferred directly on the main menu of the game.
    (Death's fade and camera's down movement are controlled with animations. Player's "wake up" and change scene is controller by the scene's script manager).
    
    [The art and the level design of the scene was already complete.]

---
+++ Escaping Death Game/Assets/Scripts/Story/EndStoryEnabler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndStoryEnabler : MonoBehaviour // End Story Manager
+{
+    public GameObject cowboyOld; // Dead Cowboy Sprite
+    public GameObject cowboyNew; // Alive (Wake Up) Cowboy Sprite
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(WakeUpCowboy());
+        StartCoroutine(ReturnToMenu());
+    }
+
+    IEnumerator WakeUpCowboy() // Enable the Alive cowboy anmd disable the dead one
+    {
+        yield return new WaitForSeconds(5.0f);
+        cowboyOld.SetActive(false);
+        cowboyNew.SetActive(true);
+    }
+
+    IEnumerator ReturnToMenu() // After the end scene the player will be tranfered directly to the main menu of the game
+    {
+        yield return new WaitForSeconds(6.5f);
+        SceneManager.LoadScene("Main Menu");
+    }
+}
