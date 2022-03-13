e20fdc058ce1eb19a84599110b4e180e746427c9
Markos <12499gr@saeinstitute.edu>
2022-03-13 23:33:13 +0200

    Minor 5
    -Minor changes on some values on the end story scene.

--- Escaping Death Game/Assets/Scripts/Story/EndStoryEnabler.cs
+++ Escaping Death Game/Assets/Scripts/Story/EndStoryEnabler.cs
@@ -16,14 +16,14 @@ public class EndStoryEnabler : MonoBehaviour // End Story Manager
 
     IEnumerator WakeUpCowboy() // Enable the Alive cowboy anmd disable the dead one
     {
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(6.5f);
         cowboyOld.SetActive(false);
         cowboyNew.SetActive(true);
     }
 
     IEnumerator ReturnToMenu() // After the end scene the player will be tranfered directly to the main menu of the game
     {
-        yield return new WaitForSeconds(6.5f);
+        yield return new WaitForSeconds(8.0f);
         SceneManager.LoadScene("Main Menu");
     }
 }
