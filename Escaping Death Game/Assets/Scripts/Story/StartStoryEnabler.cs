c2430386fe9b46da41cbfba8647aa7624eec5dc7
Markos <12499gr@saeinstitute.edu>
2022-03-14 23:13:10 +0200

    Story 4
    -The start scene starts showing the sky and the moon while the story text appear (goes up with adjustable speed thanks to a script that i created), after the player has read the story the camera will go (down) with animation on the main part of the scene showing the player and the death. After this the palyer will be tranferred directly on the level 1 of the game.
    
    (The art and the level design of the scene is ready and complete.)
    
    [Needs only the final story text to be added and finalize the values].

--- Escaping Death Game/Assets/Scripts/Story/StartStoryEnabler.cs
+++ Escaping Death Game/Assets/Scripts/Story/StartStoryEnabler.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class StartStoryEnabler : MonoBehaviour // Start Story Manager
 {
+    public float changeSceneTime; // How much time has to passed in order to tranfer the plyaer to level 1
+
     // Update is called once per frame
     void Update()
     {
@@ -12,7 +14,7 @@ public class StartStoryEnabler : MonoBehaviour // Start Story Manager
 
     IEnumerator StartGame() // After the start scene the player will be tranfered directly to the level 1 of the game
     {
-        yield return new WaitForSeconds(10.0f);
+        yield return new WaitForSeconds(changeSceneTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Escaping Death Game/Assets/Scripts/Story/TextStoryMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TextStoryMovement : MonoBehaviour // In order to control the speed and direction of the story text (how it will be appeared on the player)
+{
+    public GameObject textStory; // Sotry Text from the canvas
+    public float textSpeed; // Story's Text Speed
+
+    // Update is called once per frame
+    void Update()
+    {
+        textStory.transform.Translate(Vector2.up * textSpeed * Time.deltaTime);  // Text Moves Up Continuously
+    }
+}
